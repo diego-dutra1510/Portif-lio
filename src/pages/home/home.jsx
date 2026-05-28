d793ca7814bae60568c9827bf9783af081934a48
Robim <diegodutradoamaral616@gmail.com>
2026-05-27 23:09:41 -0300

    V0.5

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,48 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { FaDiscord, FaYoutube, FaInstagram } from "react-icons/fa";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useMotionValue } from "framer-motion";
 import "./home.css";
 
 function Home() {
+
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(0);
+
+    const [cursorVariant, setCursorVariant] = useState("default");
+
+    useEffect(() => {
+
+        const moveMouse = (e) => {
+            mouseX.set(e.clientX);
+            mouseY.set(e.clientY);
+        };
+
+        window.addEventListener(
+            "mousemove",
+            moveMouse
+        );
+
+        return () => {
+            window.removeEventListener(
+                "mousemove",
+                moveMouse
+            );
+        };
+    }, []);
+
+    const variants = {
+        default: {
+            width: 20,
+            height: 20,
+            backgroundColor: "#ffffff",
+        },
+
+        hover: {
+            width: 40,
+            height: 40,
+            backgroundColor: "rgb(241, 228, 255)",
+        }
+    };
 
     const gotas = useMemo(() => {
         return Array.from({ length: 50 }, () => ({
@@ -23,84 +62,117 @@ function Home() {
     }
 
     return (
-        <main className="container-home">
-            <div name="void" style={{ height: "12vh" }} />
-            <motion.section className="secao_atuacao">
-                {gotas.map((gota, i) => (
-                    <motion.div
-                        key={i}
-                        className="gota"
-                        initial={{
-                            y: 500,
-                            opacity: 0,
-                        }}
-                        animate={{
-                            y: -1900,
-                            opacity: [0, 1, 1, 0],
-                        }}
-                        transition={{
-                            duration: gota.duration,
-                            repeat: Infinity,
-                            delay: gota.delay,
-                            ease: "linear",
-                        }}
-                        style={{
-                            left: `${gota.left}%`,
-                            width: `${gota.size}px`,
-                            height: `${gota.size}px`,
-                        }}
-                    />
-                ))}
-            </motion.section>
-            <h1 className="atuacao">TÉCNICO EM DESENVOLVIMENTO DE SISTEMAS</h1>
-            <div className="Objetivos">
+        <>
+            <motion.div
+                className="cursor"
+                variants={variants}
+                animate={cursorVariant}
+                style={{
+                    x: mouseX,
+                    y: mouseY,
+                    translateX: "-50%",
+                    translateY: "-50%",
+                }}
+            />
 
-                <motion.div
-                    className="container"
-                    initial={{ opacity: 0, x: -100, }}
-                    whileInView={{
-                        opacity: 1, x: 0, transition: {
-                            duration: 1,
-                            delay: 4,
-                            ease: [0.22, 1, 0.36, 1]
-                        }
-                    }}
-                    whileHover={{
-                        y: -8,
-                        scale: 1.02,
-                        transition: {
-                            duration: 0.2
-                        }
-                    }}
+            <main className="container-home">
+                <div name="void" style={{ height: "12vh" }} />
+                <motion.section className="secao_atuacao">
+                    {gotas.map((gota, i) => (
+                        <motion.div
+                            key={i}
+                            className="gota"
+                            initial={{
+                                y: 500,
+                                opacity: 0,
+                            }}
+                            animate={{
+                                y: -1900,
+                                opacity: [0, 1, 1, 0],
+                            }}
+                            transition={{
+                                duration: gota.duration,
+                                repeat: Infinity,
+                                delay: gota.delay,
+                                ease: "linear",
+                            }}
+                            style={{
+                                left: `${gota.left}%`,
+                                width: `${gota.size}px`,
+                                height: `${gota.size}px`,
+                            }}
+                        />
+                    ))}
+                </motion.section>
+                <div className="content-wrapper">
+                    <div className="left-side">
+                        <h1 className="atuacao">TÉCNICO EM DESENVOLVIMENTO DE SISTEMAS</h1>
+                        <div className="Objetivos">
 
-                    viewport={{ once: true }}
-                >
-                    <h2 className="title">OBJETIVOS</h2>
-                    <p>
-                        Tenho aspirações de crescer continuamente no mercado de tecnologia, buscando sempre aprimorar minhas habilidades técnicas e desenvolver soluções inovadoras que possam transformar e otimizar processos. Meu objetivo é contribuir com conhecimento, criatividade e dedicação para impulsionar projetos que façam a diferença no dia a dia das pessoas e das empresas. Quero agregar valor ao mercado de tecnologia por meio da colaboração, aprendizado constante e da aplicação prática de novas tecnologias, promovendo impactos positivos e sustentáveis no setor.
-                    </p>
-                </motion.div>
+                            <motion.div
+                                className="container"
+                                initial={{ opacity: 0, x: -100, }}
+                                whileInView={{
+                                    opacity: 1, x: 0, transition: {
+                                        duration: 1,
+                                        delay: 3,
+                                        ease: [0.22, 1, 0.36, 1]
+                                    }
+                                }}
+                                whileHover={{
+                                    y: -8,
+                                    scale: 1.02,
+                                    transition: {
+                                        duration: 0.3
+                                    }
+                                }}
 
-                <motion.div
-                    className="model"
-                    initial={{ opacity: 0, x: 100, scale: 0.8 }}
-                    whileInView={{
-                        opacity: 1,
-                        x: 0,
-                        scale: 1
-                    }}
-                    transition={{
-                        duration: 1.2,
-                        ease: [0.22, 1, 0.36, 1]
-                    }}
-                    viewport={{ once: true }}
-                >
+                                viewport={{ once: true }}
 
-                </motion.div>
+                            >
+                                <h2 className="title">OBJETIVOS</h2>
+                                <p
+                                    onMouseEnter={() =>
+                                        setCursorVariant("hover")
+                                    }
 
-            </div>
+                                    onMouseLeave={() =>
+                                        setCursorVariant("default")
+                                    }>
+                                    Tenho aspirações de crescer continuamente no mercado de tecnologia, buscando sempre aprimorar minhas habilidades técnicas e desenvolver soluções inovadoras que possam transformar e otimizar processos. Meu objetivo é contribuir com conhecimento, criatividade e dedicação para impulsionar projetos que façam a diferença no dia a dia das pessoas e das empresas. Quero agregar valor ao mercado de tecnologia por meio da colaboração, aprendizado constante e da aplicação prática de novas tecnologias, promovendo impactos positivos e sustentáveis no setor.
+                                </p>
+                            </motion.div>
 
-            {/* <button onClick={() => div_open("objetivo")}>Objetivos</button>
+                        </div>
+                    </div>
+
+                    <div className="right-side">
+                        <div className="Card">
+                            <div className="Card_header">
+                                <div style={{
+                                    width: 20,
+                                    height: 20,
+                                    borderRadius: "50%",
+                                    backgroundColor: "#d30000",
+                                }} />
+                                <div style={{
+                                    width: 20,
+                                    height: 20,
+                                    borderRadius: "50%",
+                                    backgroundColor: "#d37800",
+                                }} />
+                                <div style={{
+                                    width: 20,
+                                    height: 20,
+                                    borderRadius: "50%",
+                                    backgroundColor: "#00e013",
+                                }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* <button onClick={() => div_open("objetivo")}>Objetivos</button>
             <AnimatePresence mode="wait">
                 {objetivo_div && (
                     <motion.div
@@ -132,7 +204,8 @@ function Home() {
             </AnimatePresence> */}
 
 
-        </main >
+            </main >
+        </>
     );
 }
 
